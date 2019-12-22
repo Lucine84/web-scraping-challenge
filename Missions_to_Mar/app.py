093b855e446841942c3dbdc82111f1b08e489057
test <test@gmail.com>
2019-12-21 19:40:15 -0800

    add

--- Missions_to_Mar/app.py
+++ Missions_to_Mar/app.py
@@ -12,3 +12,27 @@ mongo = PyMongo(app, uri="mongodb://localhost:27017/weather_app")
 # Route to render index.html template using data from Mongo
 @app.route("/")
 def home():
+
+    # Find one record of data from the mongo database
+    Mars = mongo.db.collection.find_one()
+
+    # Return template and data
+    return render_template("index.html", Mars=Mars)
+
+
+# Route that will trigger the scrape function
+@app.route("/scrape")
+def scrape():
+
+    # Run the scrape function
+    Mars_Data1 = Scrape_Mars.scrape_info()
+
+    # Update the Mongo database using update and upsert=True
+    mongo.db.collection.update({}, Mars_Data1, upsert=True)
+
+    # Redirect back to home page
+    return redirect("/")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
